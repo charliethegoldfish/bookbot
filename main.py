110b492e2b10c0d2bdfd0e6232d1983a90226688
charliethegoldfish <charliefrancis.cassidy@gmail.com>
2026-01-02 16:54:07 +1100

    take in a filepath as an arg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_char_counts, get_char_list
 
 def get_book_text(filepath):
@@ -7,7 +8,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     num_words = get_num_words(book_text)
     # print(f"Found {num_words} total words")
